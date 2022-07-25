24f10f33bed0e7cf0988e818243f15b610a2f835
zdleek <zhangbd@gmail.com>
2022-07-25 11:24:27 +0800

    Add files via upload

---
+++ GrpcClient/GrpcClient.cpp
@@ -0,0 +1,66 @@
+﻿// greeter_client.cc
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include "helloworld.grpc.pb.h"
+#endif
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using GrpcLibrary::HelloRequest;
+using GrpcLibrary::HelloReply;
+using GrpcLibrary::GrpcService;
+
+class GreeterClient {
+public:
+    GreeterClient(std::shared_ptr<Channel> channel)
+        : stub_(GrpcService::NewStub(channel)) {}
+
+
+    std::string SayHello(const std::string& user) {
+        // Data we are sending to the server.
+        HelloRequest request;
+        request.set_name(user);
+
+        // Container for the data we expect from the server.
+        HelloReply reply;
+
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        ClientContext context;
+
+        // The actual RPC.
+        Status status = stub_->SayHello(&context, request, &reply);
+
+        // Act upon its status.
+        if (status.ok()) {
+            return reply.message();
+        }
+        else {
+            std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+            return "RPC failed";
+        }
+    }
+
+private:
+    std::unique_ptr<GrpcService::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+
+    GreeterClient greeter(grpc::CreateChannel(
+        "localhost:50051", grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    std::string reply = greeter.SayHello(user);
+    std::cout << "Greeter received: " << reply << std::endl;
+    system("pause");
+    return 0;
+}
